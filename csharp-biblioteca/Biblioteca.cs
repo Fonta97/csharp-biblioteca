842442378c949255a55a8390bfdf6ef023776bc0
Fonta97 <fontana.alessandro97@gmail.com>
2024-04-23 09:18:55 +0200

    test

---
+++ csharp-biblioteca/Biblioteca.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_biblioteca
+{
+
+
+    //CLASSE UTENTE
+    public class Utente
+    {
+        private string _cognome;
+        public string Cognome
+        {
+            get { return _cognome; }
+            set { _cognome = value; }
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        private string _recapitoTelefonico;
+        public string RecapitoTelefonico
+        {
+            get { return _recapitoTelefonico; }
+            set { _recapitoTelefonico = value; }
+        }
+
+
+        public Utente(string cognome, string nome, string email, string password, string recapitoTelefonico)
+        {
+            Cognome = cognome;
+            Nome = nome;
+            Email = email;
+            Password = password;
+            RecapitoTelefonico = recapitoTelefonico;
+        }
+    }
+
+
+    //CLASSE DOC
+    public class Documento
+    {
+        private string _codiceIdentificativo;
+        public string CodiceIdentificativo
+        {
+            get { return _codiceIdentificativo; }
+            set { _codiceIdentificativo = value; }
+        }
+
+        private string _titolo;
+        public string Titolo
+        {
+            get { return _titolo; }
+            set { _titolo = value; }
+        }
+
+        private int _anno;
+        public int Anno
+        {
+            get { return _anno; }
+            set { _anno = value; }
+        }
+
+        private string _settore;
+        public string Settore
+        {
+            get { return _settore; }
+            set { _settore = value; }
+        }
+
+        private string _scaffale;
+        public string Scaffale
+        {
+            get { return _scaffale; }
+            set { _scaffale = value; }
+        }
+
+
+        //IMPORT AUTORE
+        private Autore _autore;
+        public Autore Autore
+        {
+            get { return _autore; }
+            set { _autore = value; }
+        }
+
+        public Documento(string codiceIdentificativo, string titolo, int anno, string settore, string scaffale, Autore autore)
+        {
+            CodiceIdentificativo = codiceIdentificativo;
+            Titolo = titolo;
+            Anno = anno;
+            Settore = settore;
+            Scaffale = scaffale;
+            Autore = autore;
+        }
+    }
+
+
+    //CLASSE LIBRO EREDITA DOCUMENTO
+    public class Libro : Documento
+    {
+        private int _numeroPagine;
+        public int NumeroPagine
+        {
+            get { return _numeroPagine; }
+            set { _numeroPagine = value; }
+        }
+
+        public Libro(string codiceIdentificativo, string titolo, int anno, string settore, string scaffale, Autore autore, int numeroPagine)
+             : base(codiceIdentificativo, titolo, anno, settore, scaffale, autore)
+        {
+            NumeroPagine = numeroPagine;
+        }
+    }
+
+    //CLASSE CD EREDITA DOCUMENTO
+    public class CD : Documento
+    {
+        private string _durata;
+        public string Durata
+        {
+            get { return _durata; }
+            set { _durata = value; }
+        }
+
+        public CD(string codiceIdentificativo, string titolo, int anno, string settore, string scaffale, Autore autore, string durata)
+            : base(codiceIdentificativo, titolo, anno, settore, scaffale, autore)
+        {
+            Durata = durata;
+        }
+    }
+
+
+    //CLASSE AUTORE
+    public class Autore
+    {
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set { _nome = value; }
+        }
+
+        private string _cognome;
+        public string Cognome
+        {
+            get { return _cognome; }
+            set { _cognome = value; }
+        }
+
+        public Autore(string nome, string cognome)
+        {
+            Nome = nome;
+            Cognome = cognome;
+        }
+    }
+
+    public class Prestito
+    {
+        public Utente Utente { get; set; }
+        public Documento Documento { get; set; }
+        public DateTime DataInizio { get; set; }
+        public DateTime DataFine { get; set; }
+
+        public Prestito(Utente utente, Documento documento, DateTime dataInizio, DateTime dataFine)
+        {
+            Utente = utente;
+            Documento = documento;
+            DataInizio = dataInizio;
+            DataFine = dataFine;
+        }
+    }
+
+    public class Biblioteca
+    {
+        public List<Documento> Documenti { get; private set; }
+        public List<Utente> Utenti { get; private set; }
+        public List<Prestito> Prestiti { get; private set; }
+
+
+       
+        public Biblioteca()
+        {
+            Documenti = new List<Documento>();
+            Utenti = new List<Utente>();
+            Prestiti = new List<Prestito>();
+        }
+
+        //METHODS
+
+        // Metodo new utente
+        public void AggiungiUtente(Utente utente)
+        {
+            Utenti.Add(utente);
+        }
+
+        // Metodo  add documento alla biblioteca
+        public void AggiungiDocumento(Documento documento)
+        {
+            Documenti.Add(documento);
+        }
+
+        // Metodo add prestito
+        public void AggiungiPrestito(Prestito prestito)
+        {
+            Prestiti.Add(prestito);
+        }
+
+
+        // ricerca di documenti per codice e titolo
+        public List<Documento> CercaDocumentoPerCodice(string codice)
+        {
+
+
+            // lista per memorizzare i documenti trovati
+            List<Documento> cercaConCodice = new List<Documento>();
+
+            // itera attraverso tutti i documenti nella biblioteca
+            foreach (Documento documento in Documenti)
+            {
+
+                // confronta il titolo del documento con il titolo fornito
+                // ignora le differenze tra maiuscole e minuscole nella ricerca
+                if (documento.CodiceIdentificativo.ToLower().Contains(codice.ToLower()))
+                {
+                    cercaConCodice.Add(documento);
+                }
+            }
+
+            // restituisce la lista dei documenti trovati
+            return cercaConCodice;
+        }
+        // Metodo per cercare documenti per titolo
+        public List<Documento> CercaDocumentiPerTitolo(string titolo)
+        {
+            // lista per memorizzare i documenti trovati
+            List<Documento> cercaConTitolo = new List<Documento>();
+
+            // itera attraverso tutti i documenti nella biblioteca
+            foreach (Documento documento in Documenti)
+            {
+                // confronta il titolo del documento con il titolo fornito
+                // ignora le differenze tra maiuscole e minuscole nella ricerca
+                if (documento.Titolo.ToLower().Contains(titolo.ToLower()))
+                {
+                    // Se il titolo è trovato, aggiungi il documento alla lista dei risultati
+                    cercaConTitolo.Add(documento);
+                }
+            }
+
+            // restituisce la lista dei documenti trovati
+            return cercaConTitolo;
+        }
+
+        // Metodo per cercare prestiti dato nome e cognome dell'utente
+        public List<Prestito> CercaPrestitiPerUtente(string nome, string cognome)
+        {
+            // lista per memorizzare i prestiti trovati
+            List<Prestito> prestitiTrovati = new List<Prestito>();
+
+            // itera attraverso tutti i prestiti nella biblioteca
+            foreach (Prestito prestito in Prestiti)
+            {
+                // confronta nome e cognome dell'utente associato al prestito con quelli forniti
+                // ignora le differenze tra maiuscole e minuscole nella ricerca
+                if (prestito.Utente.Nome.ToLower() == nome.ToLower() && prestito.Utente.Cognome.ToLower() == cognome.ToLower())
+                {
+                    // Se l'utente è trovato, aggiungi il prestito alla lista dei risultati
+                    prestitiTrovati.Add(prestito);
+                }
+            }
+
+            // restituisce la lista dei prestiti trovati
+            return prestitiTrovati;
+        }
+
+
+    }
+
+}
--- csharp-biblioteca/Program.cs
+++ csharp-biblioteca/Program.cs
@@ -1,88 +1,208 @@
-﻿namespace csharp_biblioteca
+﻿using System;
+
+namespace csharp_biblioteca
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            internal class Program
-        {
-            static void Main(string[] args)
+
+            //utente base Paolo Pazzo IT e Back in Black per test
+            
+            Biblioteca biblioteca = new Biblioteca();
+
+            // aggiunta di libri e CD
+            biblioteca.AggiungiDocumento(new Libro("IBN001", "It", 1986, "Horror", "A1", new Autore("Stephen", "King"), 1138));
+            biblioteca.AggiungiDocumento(new Libro("IBN002", "Harry Potter and the Philosopher's Stone", 1997, "Fantasy", "B2", new Autore("J.K.", "Rowling"), 223));
+            biblioteca.AggiungiDocumento(new Libro("IBN004", "The Lord of the Rings", 1954, "Fantasy", "A2", new Autore("J.R.R.", "Tolkien"), 1178));
+            biblioteca.AggiungiDocumento(new Libro("IBN005", "1984", 1949, "Dystopian", "B3", new Autore("George", "Orwell"), 328));
+            biblioteca.AggiungiDocumento(new CD("IBN003", "The Shawshank Redemption", 1994, "Drama", "C3", new Autore("Frank", "Darabont"), "2 ore"));
+            biblioteca.AggiungiDocumento(new CD("IBN006", "The Dark Side of the Moon", 1973, "Rock", "C4", new Autore("Pink", "Floyd"), "43 min"));
+            biblioteca.AggiungiDocumento(new CD("IBN007", "Thriller", 1982, "Pop", "C5", new Autore("Michael", "Jackson"), "42 min"));
+            biblioteca.AggiungiDocumento(new CD("IBN008", "Back in Black", 1980, "Rock", "C6", new Autore("AC", "DC"), "42 min"));
+
+
+            // aggiunta utenti preimpostati
+            Utente paolinoPazzo = new Utente("Pazzo", "Paolino", "paolino.pazzo@example.com", "password123", "1234567890");
+            biblioteca.AggiungiUtente(paolinoPazzo);
+            Utente luigiVerdi = new Utente("Verdi", "Luigi", "luigi.verdi@example.com", "password456", "0987654321");
+            biblioteca.AggiungiUtente(luigiVerdi);
+            Utente giovannaBianchi = new Utente("Bianchi", "Giovanna", "giovanna.bianchi@example.com", "password789", "5432167890");
+            biblioteca.AggiungiUtente(giovannaBianchi);
+
+            
+            Prestito prestito = new Prestito(paolinoPazzo, biblioteca.Documenti[0], DateTime.Now.AddDays(-14), DateTime.Now.AddDays(14));
+            biblioteca.AggiungiPrestito(prestito);
+
+            Prestito prestito2 = new Prestito(giovannaBianchi, biblioteca.Documenti[7], DateTime.Now.AddDays(-17), DateTime.Now.AddDays(11));
+            biblioteca.AggiungiPrestito(prestito2);
+
+            Console.WriteLine("Inserisci un nome e cognome tra gli utenti pre-selezionati per verificare se un libro è stato preso in prestito:");
+            Console.WriteLine("********************");
+            Console.WriteLine("Inserisci nome dell'utente:");
+            string nomeUtente = Console.ReadLine();
+            Console.WriteLine("Inserisci cognome dell'utente:");
+            string cognomeUtente = Console.ReadLine();
+
+            // ricerca dei prestiti per nome e cognome dell'utente
+            var prestitiUtente = biblioteca.CercaPrestitiPerUtente(nomeUtente, cognomeUtente);
+
+            // visualizzazione dei prestiti trovati
+            if (prestitiUtente.Count > 0)
             {
-                // creazione di una nuova istanza della classe Biblioteca
-                Biblioteca biblioteca = new Biblioteca();
+                Console.WriteLine($"I prestiti di {nomeUtente} {cognomeUtente} sono questi:");
+                foreach (Prestito p in prestitiUtente)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"- Titolo: {p.Documento.Titolo}," +
+                        $" \nData Inizio: {p.DataInizio.ToShortDateString()}," +
+                        $" \nData Fine: {p.DataFine.ToShortDateString()} - " +
+                        $"\nEmail: {p.Utente.Email} - " +
+                        $"\nNumero di telefono: {p.Utente.RecapitoTelefonico}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Nessun prestito trovato per {nomeUtente} {cognomeUtente}.");
+            }
 
-                // aggiunta di alcuni documenti (libri e CD) e utenti di esempio
-                biblioteca.AggiungiDocumento(new Libro("IBN001", "It", 1986, "Horror", "A1", new Autore("Stephen", "King"), 1138));
-                biblioteca.AggiungiDocumento(new Libro("IBN002", "Harry Potter and the Philosopher's Stone", 1997, "Fantasy", "B2", new Autore("J.K.", "Rowling"), 223));
-                biblioteca.AggiungiDocumento(new CD("IBN003", "The Shawshank Redemption", 1994, "Drama", "C3", new Autore("Frank", "Darabont"), "2 ore"));
+            Console.WriteLine();
+            // visualizzazione dell'elenco dei documenti nella biblioteca
+            Console.WriteLine("Libri e CD nella biblioteca:");
+            foreach (Documento documento in biblioteca.Documenti)
+            {
+                // stampa dei dettagli di ciascun documento
+                Console.WriteLine($"- {documento.CodiceIdentificativo}-{documento.Titolo} ({documento.GetType().Name})");
+            }
 
-                // aggiunta di utenti
-                Utente marioRossi = new Utente("Rossi", "Mario", "mario.rossi@example.com", "password123", "1234567890");
-                biblioteca.AggiungiUtente(marioRossi);
+            // richiesta all'utente di digitare il titolo o il codice del documento da cercare
+            Console.WriteLine("\nDigita il titolo o il codice del film o del libro che desideri cercare:");
+            string titoloOcodice = Console.ReadLine();
 
-                // associazione di un utente ad almeno un libro in questo caso IT
-                Prestito prestito = new Prestito(marioRossi, biblioteca.Documenti[0], DateTime.Now.AddDays(-14), DateTime.Now.AddDays(14));
-                biblioteca.AggiungiPrestito(prestito);
+            // ricerca dei documenti per titolo o codice
+            var nomeTrovato = biblioteca.CercaDocumentiPerTitolo(titoloOcodice);
+            var codiceTrovato = biblioteca.CercaDocumentoPerCodice(titoloOcodice);
 
-                // visualizzazione dell'elenco dei documenti nella biblioteca
-                Console.WriteLine("Documenti nella biblioteca:");
-                foreach (Documento documento in biblioteca.Documenti)
+            // verifica se sono stati trovati documenti corrispondenti
+            if (nomeTrovato.Count > 0)
+            {
+                // se sono stati trovati documenti corrispondenti per titolo
+                Console.WriteLine($"\nDocumenti trovati per il titolo '{titoloOcodice}':");
+                foreach (Documento documento in nomeTrovato)
                 {
                     // stampa dei dettagli di ciascun documento
-                    Console.WriteLine($"- {documento.CodiceIdentificativo}-{documento.Titolo} ({documento.GetType().Name})");
-                }
+                    Console.WriteLine($"-Titolo: {documento.Titolo}");
+                    Console.WriteLine($"Tipo: {documento.GetType().Name}");
+                    Console.WriteLine($"Codice: {documento.CodiceIdentificativo}");
+                    Console.WriteLine($"Anno: {documento.Anno}");
+                    Console.WriteLine($"Settore: {documento.Settore}");
+                    Console.WriteLine($"Scaffale: {documento.Scaffale}");
+                    Console.WriteLine($"Autore: {documento.Autore.Nome} {documento.Autore.Cognome}");
 
-                // richiesta all'utente di digitare il titolo o il codice del documento da cercare
-                Console.WriteLine("\nDigita il titolo o il codice del film o del libro che desideri cercare:");
-                string titoloOcodice = Console.ReadLine();
 
-                // ricerca dei documenti per titolo o codice
-                var nomeTrovato = biblioteca.CercaDocumentiPerTitolo(titoloOcodice);
-                var codiceTrovato = biblioteca.CercaDocumentoPerCodice(titoloOcodice);
-
-                // verifica se sono stati trovati documenti corrispondenti
-                if (nomeTrovato.Count > 0)
-                {
-                    // se sono stati trovati documenti corrispondenti per titolo
-                    Console.WriteLine($"\nDocumenti trovati per il titolo '{titoloOcodice}':");
-                    foreach (Documento documento in nomeTrovato)
+                    // Verifica se il documento è un CD e stampa la durata se lo è
+                    if (documento is CD cd)
                     {
-                        // stampa dei dettagli di ciascun documento
-                        Console.WriteLine($"-Titolo: {documento.Titolo}");
-                        Console.WriteLine($"Tipo: {documento.GetType().Name}");
-                        Console.WriteLine($"Codice: {documento.CodiceIdentificativo}");
-                        Console.WriteLine($"Anno: {documento.Anno}");
-                        Console.WriteLine($"Settore: {documento.Settore}");
-                        Console.WriteLine($"Scaffale: {documento.Scaffale}");
-                        Console.WriteLine($"Autore: {documento.Autore.Nome} {documento.Autore.Cognome}");
+                        Console.WriteLine($"Durata: {cd.Durata}");
+                    }
+                    // Verifica se il documento è un libro e stampa il numero di pagine se lo è
+                    else if (documento is Libro libro)
+                    {
+                        Console.WriteLine($"Numero di pagine: {libro.NumeroPagine}");
+                    }
 
 
-                        // Verifica se il documento è un CD e stampa la durata se lo è
-                        if (documento is CD cd)
+                    // verifica se il documento è stato preso in prestito da qualche utente
+                    bool presoInPrestito = false;
+                    foreach (Prestito p in biblioteca.Prestiti)
+                    {
+                        // Se il documento è stato preso in prestito, stampa i dettagli dell'utente che lo ha preso
+                        if (p.Documento.CodiceIdentificativo == documento.CodiceIdentificativo)
                         {
-                            Console.WriteLine($"Durata: {cd.Durata}");
-                        }
-                        // Verifica se il documento è un libro e stampa il numero di pagine se lo è
-                        else if (documento is Libro libro)
-                        {
-                            Console.WriteLine($"Numero di pagine: {libro.NumeroPagine}");
-                        }
+                            presoInPrestito = true;
+                            Console.WriteLine($"=======================:");
+                            Console.WriteLine($"PRESO IN PRESTITO DA:");
+                            Console.WriteLine($"- Nome e Cognome: {p.Utente.Nome} {p.Utente.Cognome}," +
+                               $" \nData Inizio: {p.DataInizio.ToShortDateString()}," +
+                               $" \nData Fine: {p.DataFine.ToShortDateString()}  " +
+                               $"\nEmail: {p.Utente.Email} - " +
+                               $"\nNumero di telefono: {p.Utente.RecapitoTelefonico}");
+                            break;
 
-
-                        // verifica se il documento è stato preso in prestito da qualche utente
-                        bool presoInPrestito = false;
-                        foreach (Prestito p in biblioteca.Prestiti)
-                        {
-                            // Se il documento è stato preso in prestito, stampa i dettagli dell'utente che lo ha preso
-                            if (p.Documento.CodiceIdentificativo == documento.CodiceIdentificativo)
-                            {
-                                presoInPrestito = true;
-                                Console.WriteLine($"Preso in prestito da: {prestito.Utente.Nome} {prestito.Utente.Cognome}");
-                                Console.WriteLine($"Data inizio prestito: {prestito.DataInizio}");
-                                Console.WriteLine($"Data fine prestito: {prestito.DataFine}");
-                                break;
-                            }
+                            //ToShortDateString = trasforma la data in stringa
                         }
                     }
+
+                    // se il documento non è stato preso in prestito
+                    if (!presoInPrestito)
+                    {
+                        Console.WriteLine("Non preso in prestito");
+                    }
+                }
+            }
+            else if (codiceTrovato.Count > 0)
+            {
+                // Se sono stati trovati documenti corrispondenti per codice
+                Console.WriteLine($"\nDocumenti trovati per il codice '{titoloOcodice}':");
+                foreach (Documento documento in codiceTrovato)
+                {
+                    // stampa dei dettagli di ciascun documento
+                    Console.WriteLine($"-Titolo: {documento.Titolo}");
+                    Console.WriteLine($"Tipo: {documento.GetType().Name}");
+                    Console.WriteLine($"Codice: {documento.CodiceIdentificativo}");
+                    Console.WriteLine($"Anno: {documento.Anno}");
+                    Console.WriteLine($"Settore: {documento.Settore}");
+                    Console.WriteLine($"Scaffale: {documento.Scaffale}");
+                    Console.WriteLine($"Autore: {documento.Autore.Nome} {documento.Autore.Cognome}");
+
+
+                    //se il documento è un CD stampa la durata 
+                    if (documento is CD cd)
+                    {
+                        Console.WriteLine($"Durata: {cd.Durata}");
+                    }
+                    //se il documento è un libro stampa il numero di pagine 
+                    else if (documento is Libro libro)
+                    {
+                        Console.WriteLine($"Numero di pagine: {libro.NumeroPagine}");
+                    }
+
+
+                    // verifica se il documento è stato preso in prestito da qualche utente
+                    bool presoInPrestito = false;
+                    foreach (Prestito p in biblioteca.Prestiti)
+                    {
+                        // Se il documento è stato preso in prestito, stampa i dettagli dell'utente che lo ha preso
+                        if (p.Documento.CodiceIdentificativo == documento.CodiceIdentificativo)
+                        {
+
+                            presoInPrestito = true;
+                            Console.WriteLine($"=======================:");
+                            Console.WriteLine($"PRESO IN PRESTITO DA:");
+                            Console.WriteLine($"- Nome e Cognome: {p.Utente.Nome} {p.Utente.Cognome}," +
+                               $" \nData Inizio: {p.DataInizio.ToShortDateString()}," +
+                               $" \nData Fine: {p.DataFine.ToShortDateString()}  " +
+                               $"\nEmail: {p.Utente.Email} - " +
+                               $"\nNumero di telefono: {p.Utente.RecapitoTelefonico}");
+                            break;
+
+                            //ToShortDateString = trasforma la data in stringa
+                        }
+
+                    }
+
+                    // se il documento non è stato preso in prestito
+                    if (!presoInPrestito)
+                    {
+                        Console.WriteLine("Non preso in prestito");
+                    }
+                }
+            }
+            else
+            {
+                // se non sono stati trovati documenti 
+                Console.WriteLine($"Nessun documento trovato per il titolo o il codice '{titoloOcodice}'");
+            }
+        }
     }
 }
